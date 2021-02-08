9b228c4d1e01334da9f0d0ae6dfe1c2b0e9ef473
iclosure <iclosure@163.com>
2021-02-08 08:56:31 +0800

    update components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-  <HelloWorld :msg="'Hello Vue ' + vueVersion + ' + Vite'" @hello="hello" />
-  <router-view></router-view>
+  <div v-if="true">
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <HelloWorld :msg="'Hello Vue ' + vueVersion + ' + Vite'" @hello="hello" />
+    <router-view></router-view>
+  </div>
+  <div v-else style="width: 100%; height: 100%">
+    <video
+      autoplay
+      muted
+      controls
+      ref="videoRef"
+      width="600"
+      height="350"
+    ></video>
+  </div>
 </template>
 
 <script lang="ts">
 import HelloWorld from "@/components/HelloWorld.vue";
 import {
   defineComponent,
+  onMounted,
   provide,
   reactive,
   ref,
   toRefs,
   version as VueVersion
 } from "vue";
+import flvjs from "flv.js";
 
 export default defineComponent({
   name: "App",
@@ -21,19 +35,48 @@ export default defineComponent({
     HelloWorld
   },
   setup() {
-    const state = reactive({
+    //
+    const initData = reactive({
       vueVersion: VueVersion
     });
 
-    function hello(msg: unknown): any {
+    const videoRef = ref<HTMLVideoElement>();
+
+    function hello(msg: string): void {
       console.log("App:", msg);
     }
 
+    //
     const appName = ref("oo-ooo");
     provide("appName", appName);
 
+    onMounted(() => {
+      if (videoRef.value) {
+        if (flvjs.isSupported()) {
+          const flvPlayer = flvjs.createPlayer(
+            {
+              type: "flv",
+              isLive: true, //直播模式
+              hasAudio: false, //关闭音频
+              hasVideo: true,
+              url: "http://localhost/live?port=1935&app=myapp&stream=mystream"
+            },
+            {
+              //enableWorker: true, //浏览器端开启flv.js的worker,多进程运行flv.js
+              stashInitialSize: 128,
+              enableStashBuffer: false //播放flv时，设置是否启用播放缓存，只在直播起作用。
+            }
+          );
+          flvPlayer.attachMediaElement(videoRef.value as HTMLVideoElement);
+          flvPlayer.load();
+          flvPlayer.play();
+        }
+      }
+    });
+
     return {
-      ...toRefs(state),
+      ...toRefs(initData),
+      videoRef,
       hello
     };
   }
